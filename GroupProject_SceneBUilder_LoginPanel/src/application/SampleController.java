6f7f946e6ba3d2f943da4e91b2527579c4b3578e
jaron <jaron@192.168.0.25>
2024-01-31 15:59:37 -0700

    Login Panel Commit

---
+++ GroupProject_SceneBUilder_LoginPanel/src/application/SampleController.java
@@ -0,0 +1,46 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+//import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
+
+public class SampleController {
+	
+		@FXML
+		private Button exitButton;
+		
+		@FXML
+		private Label loginMessageLabel;
+		
+		@FXML
+		private TextField usernameTextField;
+		
+		@FXML
+		private PasswordField passwordTextField;
+		
+	
+		
+		
+		
+		
+		public void exitButtonOnAction(ActionEvent e) {
+			Stage stage = (Stage) exitButton.getScene().getWindow();
+			stage.close();
+		
+		}
+		
+	
+	
+		
+		
+	
+		
+	}
+
+
+
---
+++ GroupProject_SceneBUilder_LoginPanel/src/module-info.java
@@ -0,0 +1,6 @@
+module GroupProject_SceneBUilder_LoginPanel {
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
