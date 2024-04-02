8136201dfdc2cd615dd6c8825241612e2eab082b
Branden Palmer <brandenpalmer08@gmail.com>
2024-04-03 01:24:57 +0700

    Add setRoot static method to main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,40 @@
 package application;
-	
+
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.fxml.FXMLLoader;
-
+import javafx.stage.Stage;
 
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
+
+	private static Scene scene;
+
+	public static void setRoot(String fxmlFileName) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Login.fxml"));
-			Scene scene = new Scene(root,550,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
+			Main.scene.setRoot(
+					FXMLLoader.load(Main.class.getClass().getResource(fxmlFileName)));
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+		BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("Login.fxml"));
+		scene = new Scene(root, 550, 400);
+		scene.getStylesheets().add(
+				getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
